ced295359f275cf64ef653cdc8a015a791417862
arvind singh <1141191arvind.singh@gamil.com>
2017-12-27 12:20:29 +0530

    now snake is terminating but it can not eat food of grid edge

--- src/main.js
+++ src/main.js
@@ -1,25 +1,27 @@
-let snake=undefined;
-let food=undefined;
-let numberOfRows=60;
-let numberOfCols=120;
+let snake = undefined;
+let food = undefined;
+let numberOfRows = 60;
+let numberOfCols = 120;
 
-let animator=undefined;
+let animator = undefined;
 
-const animateSnake=function() {
-  let oldHead=snake.getHead();
-  let oldTail=snake.move();
-  let head=snake.getHead();
+const animateSnake = function() {
+  let oldHead = snake.getHead();
+  let oldTail = snake.move();
+  let head = snake.getHead();
+  console.log(head);
   paintBody(oldHead);
   unpaintSnake(oldTail);
   paintHead(head);
-  if(head.isSameCoordAs(food)) {
+  if (head.isSameCoordAs(food)) {
     snake.grow();
-    createFood(numberOfRows,numberOfCols);
+    createFood(numberOfRows, numberOfCols);
     drawFood(food);
   }
+  checkForLoose(head);
 }
 
-const changeSnakeDirection=function(event) {
+const changeSnakeDirection = function(event) {
   switch (event.code) {
     case "KeyA":
       snake.turnLeft();
@@ -34,34 +36,42 @@ const changeSnakeDirection=function(event) {
   }
 }
 
-const addKeyListener=function() {
-  let grid=document.getElementById("keys");
-  grid.onkeyup=changeSnakeDirection;
+const addKeyListener = function() {
+  let grid = document.getElementById("keys");
+  grid.onkeyup = changeSnakeDirection;
   grid.focus();
 }
 
-const createSnake=function() {
-  let tail=new Position(12,10,"east");
-  let body=[];
+const createSnake = function() {
+  let tail = new Position(12, 10, "south");
+  let body = [];
   body.push(tail);
   body.push(tail.next());
-  let head=tail.next().next();
+  let head = tail.next().next();
 
-  snake=new Snake(head,body);
+  snake = new Snake(head, body);
 }
 
-const createFood=function(numberOfRows,numberOfCols) {
-  food=generateRandomPosition(numberOfCols,numberOfRows);
+const createFood = function(numberOfRows, numberOfCols) {
+  food = generateRandomPosition(numberOfCols, numberOfRows);
 }
 
-const startGame=function() {
+const startGame = function() {
   createSnake();
-  drawGrids(numberOfRows,numberOfCols);
+  drawGrids(numberOfRows, numberOfCols);
   drawSnake(snake);
-  createFood(numberOfRows,numberOfCols);
+  createFood(numberOfRows, numberOfCols);
   drawFood(food);
   addKeyListener();
-  animator=setInterval(animateSnake,140);
+  animator = setInterval(animateSnake, 140);
 }
 
-window.onload=startGame;
+const checkForLoose = function(head) {
+  if(head.y==0||head.x==0){
+    snake = new Snake(null,null);
+  }
+  if(head.x==numberOfCols-1||head.y==numberOfRows-1){
+    snake = new Snake(null,null);
+  }
+}
+window.onload = startGame;
